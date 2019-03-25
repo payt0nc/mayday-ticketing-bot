18d65fb8ed1cf1a035dea4e0610567eca8228e43
Cooomma <payton@cooomma.info>
2019-03-25 23:12:37 +0800

    fix python import bug

--- mayday/helpers/__init__.py
+++ mayday/helpers/__init__.py
@@ -1,8 +1,9 @@
 from .auth_helper import AuthHelper
 from .feature_helpers import FeatureHelper
+from .query_helper import QueryHelper
+from .ticket_helper import TicketHelper
+
 from .feature_helpers.post_ticket_helper import PostTicketHelper
 from .feature_helpers.quick_search_helper import QuickSearchHelper
 from .feature_helpers.search_helper import SearchHelper
 from .feature_helpers.update_helper import UpdateHelper
-from .query_helper import QueryHelper
-from .ticket_helper import TicketHelper
