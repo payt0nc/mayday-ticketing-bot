d499ccec8c32991144d1910f0c82e5b05f1a5117
Cooomma <payton@cooomma.info>
2019-04-10 01:20:06 +0800

    test generate chart

--- mayday/features/events.py
+++ mayday/features/events.py
@@ -1,11 +1,12 @@
 import logging
 import time
 
+from telegram import chataction
 from telegram.ext.dispatcher import run_async
 from telegram.parsemode import ParseMode
 
 import mayday
-from mayday.constants import conversations, stages
+from mayday.constants import conversations, stages, STATUS_MAPPING
 from mayday.constants.replykeyboards import KEYBOARDS
 from mayday.helpers.feature_helpers.events_helper import EventHelper
 from mayday.objects.user import User
@@ -44,4 +45,31 @@ def info(bot, update, *args, **kwargs):
         chat_id=user.user_id,
         photo=conversations.SEATING_PLAN,
         parse_mode=ParseMode.MARKDOWN)
+    return stages.END
+
+
+@run_async
+def send_chart(bot, update, *args, **kwargs):
+    user = User(telegram_user=update.effective_user)
+    bot.send_chat_action(chat_id=user.user_id, action=chataction.ChatAction.TYPING)
+    stats = event_helper.generate_charts()
+
+    if not (stats.get('status_distribution') or (stats.get('ticket_charts'))):
+        bot.send_chart(chat_id=user.user_id, text=conversations.STATS_NONE)
+        return stages.END
+
+    if stats.get('status_distribution'):
+        bot.send_message(
+            chat_id=user.user_id,
+            text=conversations.STATS.format(
+                updated_at=stats['updated_at'],
+                status_distribution='\n'.join(
+                    [conversations.STATUS_STAT.format(status=STATUS_MAPPING.get(status_id), amount=amount)
+                     for status_id, amount in stats.get('status_distribution').items()])))
+        bot.send_chat_action(chat_id=user.user_id, action=chataction.ChatAction.TYPING)
+
+    if stats.get('ticket_charts'):
+        for chart in stats['ticket_charts']:
+            bot.send_photo(chat_id=user.user_id, photo=open(chart, 'rb'))
+            time.sleep(0.2)
     return stages.END
